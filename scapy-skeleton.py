0503f10e1802ee1a7ead0ef3d5382515d2187752
Kenton Carrier <kncarrier28@gmail.com>
2019-03-29 12:33:51 -0400

    Identify flows from the packet sniffer

--- scapy-skeleton.py
+++ scapy-skeleton.py
@@ -4,18 +4,47 @@ import numpy as np
 import sys
 import socket
 import os
+import csv
+
+class Flow:
+    def __init__(self, pkt):
+        self.proto = pkt[1].proto
+        self.srcIp = pkt[1].src
+        self.dstIp = pkt[1].dst
+        self.srcPort = pkt[2].sport
+        self.dstPort = pkt[2].dport
+
+    def isPartOfFlow(self, pkt):
+        isInFlow = False
+        if pkt[1].proto == self.proto:
+            isInFlow = (pkt[1].src == self.srcIp and pkt[1].dst == self.dstIp) or (pkt[1].src == self.dstIp and pkt[1].dst == self.srcIp)
+            if isInFlow:
+                isInFlow = (pkt[1].sport == self.srcPort and pkt[1].dport == self.dstPort) or (pkt[1].sport == self.dstPort and pkt[1].dport == self.srcPort)
+        return isInFlow
+
+    def calculateFeatures():
+        print("test")
 
 def fields_extraction(x):
-    #x.sprintf("{IP:%IP.src%,%IP.dst%,}"
-        #"{TCP:%TCP.sport%,%TCP.dport%,}"
-        #"{UDP:%UDP.sport%,%UDP.dport%}")
+    print(x.sprintf("{IP:%IP.src%, %IP.dst%, }"
+            "{TCP:%TCP.sport%, %TCP.dport%, }"
+            "{UDP:%UDP.sport%, %UDP.dport%}"))
 
-    #print x.summary()
+flows = []
 
-    x.show()
+pkts = sniff(filter = "tcp or udp", prn = fields_extraction, count = 1000)
 
-    #use x.time for time information on the pkts
+print("\nPackets Sniffed: ", len(pkts))
 
-pkts = sniff(prn = fields_extraction, count = 10)
+flows.append(Flow(pkts[0]))
+pkts.pop(0)
 
-# print pkts[0].show()
+for pkt in pkts:
+    inAFlow = False
+    for flow in flows:
+        if flow.isPartOfFlow(pkt) == True:
+            inAFlow = True
+    if inAFlow == False:
+        flows.append(Flow(pkt))
+
+print("Number of Detected Flows: ", len(flows))
