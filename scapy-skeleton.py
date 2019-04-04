cae9ec2010fe87710231629a2ee7251ec46bb51c
Kenton Carrier <kncarrier28@gmail.com>
2019-04-04 15:57:40 -0400

    Add ack time as a feature

--- scapy-skeleton.py
+++ scapy-skeleton.py
@@ -5,28 +5,66 @@ import sys
 import socket
 import os
 import csv
+import statistics
 
 class Flow:
     def __init__(self, pkt):
         self.proto = pkt[1].proto
+        if pkt[1].proto == 6:
+            self.avgAckTime = -1
         self.srcIp = pkt[1].src
         self.dstIp = pkt[1].dst
         self.srcPort = pkt[2].sport
         self.dstPort = pkt[2].dport
+        self.avgSize = pkt[1].len
+        self.avgTtl = pkt[1].ttl
+        self.pkts = [pkt]
+        self.numPkts = 1
 
     def isPartOfFlow(self, pkt):
-        isInFlow = False
         if pkt[1].proto == self.proto:
-            isInFlow = (pkt[1].src == self.srcIp and pkt[1].dst == self.dstIp) or (pkt[1].src == self.dstIp and pkt[1].dst == self.srcIp)
-            if isInFlow:
-                isInFlow = (pkt[1].sport == self.srcPort and pkt[1].dport == self.dstPort) or (pkt[1].sport == self.dstPort and pkt[1].dport == self.srcPort)
-        return isInFlow
+            if (pkt[1].src == self.srcIp and pkt[1].dst == self.dstIp) or (pkt[1].src == self.dstIp and pkt[1].dst == self.srcIp):
+                if (pkt[1].sport == self.srcPort and pkt[1].dport == self.dstPort) or (pkt[1].sport == self.dstPort and pkt[1].dport == self.srcPort):
+                    self.calcFeatures(pkt)
+                    if self.proto == 6:
+                        self.checkForAck(pkt)
+                    self.pkts.append(pkt)
+                    self.numPkts += 1
+                    return True
+        return False
 
-    def calculateFeatures():
-        print("test")
+    def calcFeatures(self, pkt):
+        self.calcAvgSize(pkt)
+        self.calcAvgTtl(pkt)
+
+    def calcAvgSize(self, pkt):
+        self.avgSize = statistics.mean([self.avgSize, pkt[1].len])
+
+    def calcAvgTtl(self, pkt):
+        self.avgTtl = statistics.mean([self.avgTtl, pkt[1].ttl])
+
+    def checkForAck(self, pkt):
+        for flowPkt in self.pkts:
+            if flowPkt[2].seq == pkt[2].ack:
+                if self.avgAckTime == -1:
+                    self.avgAckTime = pkt.time - flowPkt.time
+                else:
+                    self.avgAckTime = statistics.mean([pkt.time - flowPkt.time, self.avgAckTime])
+                self.pkts.pop(self.pkts.index(flowPkt))
+
+    def printFeatures(self):
+        print("Number of Packets in flow: ", self.numPkts)
+        print("Average size of packets: ", self.avgSize)
+        print("Average time to live of packets: ", self.avgTtl)
+        print("Protocol used: ", self.proto)
+        if self.proto == 6:
+            if self.avgAckTime == -1:
+                print("No acks received, try sniffing more packets")
+            else:
+                print("Average time to ack: ", self.avgAckTime, " seconds")
 
 def fields_extraction(x):
-    print(x.sprintf("{IP:%IP.src%, %IP.dst%, }"
+    print(x.sprintf("{IP:%IP.src%, %IP.dst%, %IP.len%, }"
             "{TCP:%TCP.sport%, %TCP.dport%, }"
             "{UDP:%UDP.sport%, %UDP.dport%}"))
 
@@ -35,9 +73,6 @@ flows = []
 pkts = sniff(filter = "tcp or udp", prn = fields_extraction, count = 1000)
 
 print("\nPackets Sniffed: ", len(pkts))
-
-flows.append(Flow(pkts[0]))
-pkts.pop(0)
 
 for pkt in pkts:
     inAFlow = False
@@ -48,3 +83,8 @@ for pkt in pkts:
         flows.append(Flow(pkt))
 
 print("Number of Detected Flows: ", len(flows))
+
+#CSV generation should go here
+for i, flow in enumerate(flows):
+    print("\nFlow id -", i)
+    flow.printFeatures()
