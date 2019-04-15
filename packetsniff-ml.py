6639b8a0a935ef6a7e64028f1fd32d64952abdcb
Kenton Carrier <kncarrier28@gmail.com>
2019-04-14 23:48:22 -0400

    Add prediction from packetsniffer-sniff.py

--- packetsniff-ml.py
+++ packetsniff-ml.py
@@ -13,6 +13,7 @@ from sklearn.datasets import make_classification
 
 # read CSV including header
 df = pd.read_csv("data/trainingSet.csv", header=0)
+pf = pd.read_csv("data/flows.csv", header=0)
 # You might not need this next line if you do not care about losing information about flow_id etc. All you actually need to
 # feed your machine learning model are features and output label.
 # columns_list = ['proto', 'feature_1', 'feature_2', 'feature_3', 'feature_4', 'label']
@@ -21,6 +22,9 @@ df = pd.read_csv("data/trainingSet.csv", header=0)
 
 X = df[df.columns[:-1]] # use all columns except last one (type)
 y = df['type']
+
+predict = pf[pf.columns]
+
 resultsDT = []
 resultsNN = []
 resultsSVC = []
@@ -33,16 +37,19 @@ for i in range(0, 10):
     clfDT = tree.DecisionTreeClassifier()
     clfDT.fit(X_train, y_train)
     resultsDT.append(clfDT.score(X_test, y_test))
+    print("DT Prediction: ", clfDT.predict(predict))
 
     # Neural network (MultiPerceptron Classifier)
     clfNN = MLPClassifier()
     clfNN.fit(X_train, y_train)
     resultsNN.append(clfNN.score(X_test, y_test))
+    print("NN Prediction: ", clfDT.predict(predict))
 
     #SVM's
     clfSVC = SVC(gamma='auto')     #SVC USE THIS
     clfSVC = LinearSVC(max_iter = 10000)  #Linear SVC
     clfSVC.fit(X_train, y_train)
+    print("SVC Prediction: ", clfDT.predict(predict))
 
     #here you are supposed to calculate the evaluation measures indicated in the project proposal (accuracy, F-score etc)
     resultsSVC.append(clfSVC.score(X_test, y_test))  #accuracy score -- score has to be output in graph form
